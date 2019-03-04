98163946ec47348ee05b87be4aca87a7ee70d2f7
alenumerek <alenumerek33@gmail.com>
2019-03-04 02:58:14 +0100

    rozwinięcie templeta

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Blog
 
 # Create your views here.
 def blog_list(request):
-    return render(request, 'blog/post_list.html', {}) #zawżyj template -> blog -> plik html
+    blogs = Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', { 'blogs': blogs }) #zawżyj template -> blog -> plik html

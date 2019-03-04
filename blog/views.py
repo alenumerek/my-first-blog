afa531b5d402dda86afe7f26296bf10fe0754aa9
alenumerek <alenumerek33@gmail.com>
2019-03-04 03:03:49 +0100

    rozwinięcie templeta

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Blog
 
 # Create your views here.
 def blog_list(request):
-    blogs = Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    blogs = Blog.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', { 'blogs': blogs }) #zawżyj template -> blog -> plik html
